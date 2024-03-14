2dad5c12e26cf050d7e3f5827e1168245355c1f6
Rishika billore <150771691+rishikabillore26@users.noreply.github.com>
2024-03-14 19:55:44 +0530

    Update hagma man game.py

--- hagma man game.py
+++ hagma man game.py
@@ -4,7 +4,6 @@ lives = 6
 chosen_word = random.choice(word_list).lower()
 print(chosen_word)
 display = ['_' if letter != ' ' else ' ' for letter in chosen_word]
-print(display)
 
 hangman_stages = [
     """
